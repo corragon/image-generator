80d3decbac2fa3b6bac602e883ca008a5c7b3328
Rachelle Yarnall <rachelley@Rachelles-MacBook-Air.local>
2017-10-14 15:44:38 -0500

    Added html and css files. Updated index.js to load html file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const config = require('./config');
 const Router = require('express').Router;
+const path = require('path');
 
 const app  = express();
 const router = new Router();
 
 router.route('/').get((req, res) => {
-  res.send('Welcome to the image generator!');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use(router);
