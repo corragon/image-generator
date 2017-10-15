58ee9f7fb852867d7670be3cbff0852c0b81c73b
Josh A <jjajaguar@gmail.com>
2017-10-14 20:15:39 -0500

    Add test code to image.js

--- image.js
+++ image.js
@@ -2,12 +2,21 @@ const Jimp = require('jimp');
 
 function generateImage(imageParams)
 {
-	var image = new Jimp(imageParams.width, imageParams.height, imageParams.backgroundColor, function (err, image) {});
+	var image = new Jimp(imageParams.width, imageParams.height, imageParams.backgroundColor);
 
 	image.write('cow.png', function (err, image) {
 		if (err) throw err;
 		console.log('Written to the file');
 	});
+	
+	return 'cow.png';
 }
+
+var ip = {};
+ip.width = 500;
+ip.height = 300;
+ip.backgroundColor = 0xFF8040FF;
+
+generateImage(ip);
 
 module.exports = generateImage;
