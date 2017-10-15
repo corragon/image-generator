78b8c90768cdcf93eb367a172512815d9382a4b3
Josh A <jjajaguar@gmail.com>
2017-10-14 19:06:38 -0500

    adds Jimp library and updates image.js to use it

--- image.js
+++ image.js
@@ -1,15 +1,16 @@
-const PNGImage = require('pngjs-image');
+const Jimp = require('jimp');
 
+var image = new Jimp(256, 256, 0x000000FF, function (err, image) {
+    // this image is 256 x 256, every pixel is set to 0x00000000
+});
 
-let image = PNGImage.createImage(100, 300);
-
-for (let i = 20; i < 30; i++) {
+/*for (let i = 20; i < 30; i++) {
   for (let j = 20; j < 30; j++) {
     image.setAt(i, j, { red:255, green:0, blue:0, alpha:255 });
   }
-}
+}*/
 
-image.writeImage('chicken.png', function (err) {
+image.write('cow.png', function (err, image) {
   if (err) throw err;
   console.log('Written to the file');
 });
